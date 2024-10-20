38a0e19d2174dd68c79e92ede91ae2e31d2fb37d
OlgaKaratkevich <volhakaratkevich@gmail.com>
2024-10-20 23:56:59 +0300

    --

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -2,8 +2,12 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
+    public static final By FIRST_STEPS_TEXT_LABEL = By.xpath("//h1[text()='Первые шаги в МоемСкладе']");
+    public static final By POPUP_PANEL = By.xpath("//div[@class = 'lognex-popup-panel']");
+    public static final By POPUP_PANEL_CLOSE_BUTTON = By.xpath("//div[@class = 'lognex-popup-panel']//button");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -11,6 +15,7 @@ public class HomePage extends BasePage{
 
     @Override
     public boolean isOpened() {
-        return driver.findElement(By.xpath("//h1[text()='Первые шаги в МоемСкладе']")).isDisplayed();
+        return isExist(FIRST_STEPS_TEXT_LABEL);
     }
+
 }
